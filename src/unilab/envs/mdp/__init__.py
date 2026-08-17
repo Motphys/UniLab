63dad7dd54953a6b4558258ff519633157890a51
YUFEI JIA <59379871+TATP-233@users.noreply.github.com>
2026-08-17 22:00:31 +0800

    feat(env): add NumPy reward terms (#1065)

--- src/unilab/envs/mdp/__init__.py
+++ src/unilab/envs/mdp/__init__.py
@@ -11,6 +11,17 @@ from unilab.envs.mdp.observations import joint_pos_rel as joint_pos_rel
 from unilab.envs.mdp.observations import joint_vel_rel as joint_vel_rel
 from unilab.envs.mdp.observations import last_action as last_action
 from unilab.envs.mdp.observations import projected_gravity as projected_gravity
+from unilab.envs.mdp.rewards import action_acc_l2 as action_acc_l2
+from unilab.envs.mdp.rewards import action_rate_l2 as action_rate_l2
+from unilab.envs.mdp.rewards import (
+    body_angular_velocity_penalty as body_angular_velocity_penalty,
+)
+from unilab.envs.mdp.rewards import flat_orientation_l2 as flat_orientation_l2
+from unilab.envs.mdp.rewards import is_alive as is_alive
+from unilab.envs.mdp.rewards import is_terminated as is_terminated
+from unilab.envs.mdp.rewards import joint_vel_l2 as joint_vel_l2
+from unilab.envs.mdp.rewards import track_angular_velocity as track_angular_velocity
+from unilab.envs.mdp.rewards import track_linear_velocity as track_linear_velocity
 from unilab.envs.mdp.terminations import bad_orientation as bad_orientation
 from unilab.envs.mdp.terminations import (
     root_height_below_minimum as root_height_below_minimum,
@@ -22,14 +33,23 @@ __all__ = [
     "JointPositionActionCfg",
     "UniformVelocityCommand",
     "UniformVelocityCommandCfg",
+    "action_acc_l2",
+    "action_rate_l2",
     "base_ang_vel",
     "base_lin_vel",
     "bad_orientation",
+    "body_angular_velocity_penalty",
+    "flat_orientation_l2",
     "generated_commands",
     "joint_pos_rel",
     "joint_vel_rel",
+    "joint_vel_l2",
     "last_action",
+    "is_alive",
+    "is_terminated",
     "projected_gravity",
     "root_height_below_minimum",
     "time_out",
+    "track_angular_velocity",
+    "track_linear_velocity",
 ]
