e275aad7ee7b01d8edbdcf5176a3addc030d0f07
TATP-233 <jyf23@mails.tsinghua.edu.cn>
2026-08-18 20:56:21 +0800

    refactor(tasks): move Go1 rough owner [skip ci]

--- src/unilab/envs/locomotion/go1/__init__.py
+++ src/unilab/envs/locomotion/go1/__init__.py
@@ -1,2 +1,1 @@
 from .joystick import Go1JoystickCfg, Go1WalkTask
-from .rough import Go1JoystickRoughCfg, Go1JoystickRoughEnv
---
+++ src/unilab/tasks/locomotion/go1/__init__.py
@@ -0,0 +1,3 @@
+from .rough import Go1JoystickRoughCfg, Go1JoystickRoughEnv
+
+__all__ = ["Go1JoystickRoughCfg", "Go1JoystickRoughEnv"]
